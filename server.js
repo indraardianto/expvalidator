86625a8544dd3079f4cc09766ed2d4e4629f0146
indraardianto <indraardianto5@gmail.com>
2024-09-08 14:42:05 +0700

    add depedensi

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import "dotenv/config";
+import sequelize from './app/config/database.js';
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('App is up and running!');
+    sequelize.authenticate().then(() => {
+        res.json({
+            message: 'Database connected successfully!'
+        })
+    }).catch((error) => {
+        console.log(error);
+    })
 });
 
 app.listen(PORT, () => {
